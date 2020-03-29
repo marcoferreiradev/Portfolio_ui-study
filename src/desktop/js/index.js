c428d1dfcb3087fd589a451b71f1597bfd60b8a1
Marco Willian Bezerra Ferreira <marco.ferreira@enext.com.br>
2020-03-29 19:48:13 -0300

    feat/ Slides funcioando, main finalizado |JS,SASS

--- src/desktop/js/index.js
+++ src/desktop/js/index.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+
+const methods = {
+    init(){
+        methods.slickGallery();
+    },
+    slickGallery(){
+        $('.portfolio__navfor--images-list').slick({
+            vertical:true,
+            slideToShow:3,
+            slideToScroll:1,
+            arrows:true,
+            asNavFor:'.portfolio__collection--main-block',
+        });
+        $('.portfolio__collection--main-block').slick({
+            slideToShow:1,
+            slideToScroll:1,
+            dots:true,
+            asNavFor:'.portfolio__navfor--images-list',
+            arrows:false,
+        });
+    }
+}
+
+// $(document).load( function() {
+//     console.log('dadasdas');
+//     methods.init();
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    methods.init();
+})
